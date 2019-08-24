b1f95e2f4728585b70cd45d6244077debbc50fc8
NovakDetti <novakbernadett94@gmail.com>
2019-08-24 15:51:47 +0200

    setup Resnet50 instead of SqueezeNet

--- SeeFood/SeeFood/ViewController.swift
+++ SeeFood/SeeFood/ViewController.swift
@@ -26,26 +26,54 @@ class ViewController: UIViewController,  UIImagePickerControllerDelegate, UINavi
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedimage = info[.originalImage] as? UIImage {
             recognizerImageView.image = userPickedimage
-            let ciiImage = CIImage(image: userPickedimage)
+            guard let ciiImage = CIImage(image: userPickedimage) else{fatalError()}
+            detect(image: ciiImage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
     }
 
-    func detect(image : CIImage){
-        guard let model = try? VNCoreMLModel(for: SqueezeNet().model) else{ fatalError()}
+    func detect(image: CIImage) {
+            guard let model = try? VNCoreMLModel(for: Resnet50().model) else {
+            fatalError("can't load ML model")
+        }
         
-        let request = VNCoreMLRequest(model: model) { (request, error) in
-            guard let results = request.results as? [VNClassificationObservation] else {fatalError()}
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first
+                else {
+                    fatalError("unexpected result type from VNCoreMLRequest")
+            }
+            
+            if topResult.identifier.contains("hotdog") {
+                DispatchQueue.main.async {
+                    self.navigationItem.title = "Hotdog!"
+                    self.navigationController?.navigationBar.barTintColor = UIColor.green
+                    self.navigationController?.navigationBar.isTranslucent = false
+                    
+                    
+                }
+            }
+            else {
+                DispatchQueue.main.async {
+                    self.navigationItem.title = "Not Hotdog!"
+                    self.navigationController?.navigationBar.barTintColor = UIColor.red
+                    self.navigationController?.navigationBar.isTranslucent = false
+                    
+                }
+            }
+            
+            
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
-        do{
-            try handler.perform([request])
-        }catch{
-            print(error)
-        }
+        
+        do { try handler.perform([request]) }
+        catch { print(error) }
+        
+        
+        
     }
    
 
